b5235a2899cf3ffcab89dc92691f63d52e35f1f6
Nikolai Nanev <niki4etooo@gmail.com>
2021-09-20 19:45:03 +0300

    add: order button

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,9 @@ import '../scss/app.scss';
 /* Your JS Code goes here */
 const coffeeList = document.getElementById("list-of-coffee-drinks");
 
+const btn_orderDrink = document.getElementById('btn_orderCoffeeDrink');
+const output_result = document.getElementById('output_result');
+
+btn_orderDrink.addEventListener('click', function(){
+	output_result.textContent = "You made successfull order! Thanks :)");
+});
