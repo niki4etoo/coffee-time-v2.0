62fc1ee249eeb6c0602410d5250ab0c21dd89da3
Nikolai Nanev <niki4etooo@gmail.com>
2021-09-22 20:59:22 +0300

    add: list of coffee drinks in Coffee class

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,18 @@
 import '../scss/app.scss';
 
+import Coffee from './coffee.js';
+
 /* Your JS Code goes here */
 const coffeeList = document.getElementById("list-of-coffee-drinks");
 
 const btn_orderDrink = document.getElementById('btn_orderCoffeeDrink');
 const output_result = document.getElementById('output_result');
 
-btn_orderDrink.addEventListener('click', function(){
-	output_result.textContent = "You made successfull order! Thanks :)";
-});
+var coffee = new Coffee();
+console.log(coffee);
+
+if(btn_orderDrink != null){
+	btn_orderDrink.addEventListener('click', function(){
+		output_result.textContent = "You made successfull order! Thanks :)";
+	});
+}
