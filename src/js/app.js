e6159806cfc79b3a61756bb7d93df5e9f4ab8446
Nikolai Nanev <niki4etooo@gmail.com>
2021-10-15 19:01:12 +0300

    add: sitemap on the footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,6 @@
 import '../scss/app.scss';
 import Coffee from './coffee.js';
 
-var coffeeList = {
-  "cappuccino": "Cappuccino",
-  "latte": "Latte",
-  "americano": "Americano",
-  "espresso": "Espresso"
-};
-
-
-
-const btn_orderDrink = document.getElementById('btn_orderCoffeeDrink');
-const output_result = document.getElementById('output_result');
-
 var coffee = new Coffee();
 
 if(btn_orderDrink != null){
