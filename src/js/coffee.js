bfb95f5bf726123bd04532a8ec2172ae1c2c45a7
Nikolai Nanev <niki4etooo@gmail.com>
2021-10-16 17:56:46 +0300

    fix: remove redunce code

--- src/js/coffee.js
+++ src/js/coffee.js
@@ -1,19 +1,7 @@
 export default class Coffee {
-	lists = {
-		mocca: "Mocca",
-		moccachino: "Moccachino",
-		latte: "Latte",
-		americano: "Americano",
-		espresso: "Espresso"
-	};
-
 	
-
 	constructor(){
-		var coffeeList = document.getElementById("coffeeList");
-		for(var l in this.lists){
-			coffeeList.innerText += l + "\n";
-		}
+		
 	}
 
 }
