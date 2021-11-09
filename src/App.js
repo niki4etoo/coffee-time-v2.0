81cb5ef66d2dde0dbd6121911e142e25b48a2d5c
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-09 19:29:31 +0200

    feat: simple intro animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import styled, { keyframes } from 'styled-components';
+
+import coffeeLogo from './images/GIF/coffee.gif';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+		<CoffeeTitle>Coffee Time</CoffeeTitle>
+		<CoffeeLogo src={coffeeLogo} width="300" height="300"></CoffeeLogo>
     </div>
   );
 }
+
+const animationCoffeeTitle = keyframes`
+	0% { opacity: 0; transform: translateY(-200px); }
+	50% { opacity: 0.5; transform: translateY(0px); }
+	100% { opacity: 1; }
+`
+
+const animationCoffeeLogo = keyframes`
+	0% { opacity: 0; transform: translateY(-200px); }
+	100% { opacity: 1; transofrm: translateY(0px); }
+`
+
+const CoffeeTitle = styled.h1`
+	animation-name: ${animationCoffeeTitle};
+	animation-duration: 2s;
+	animation-fill-mode: forwards;
+`
+
+const CoffeeLogo = styled.img`
+	opacity: 0;
+	animation-name: ${animationCoffeeLogo};
+	animation-duration: 2s;
+	animation-delay: 2s;
+	animation-fill-mode: forwards;
+`
 
 export default App;
