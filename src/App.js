c84fe5bd436970a1d6701b068474f0a2f969b4b9
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-13 12:06:13 +0200

    feat: intro of the coffee time app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import styled, { keyframes } from 'styled-components';
-
-import coffeeLogo from './images/GIF/coffee.gif';
-
 import './App.css';
+
+import CoffeeLogo from './components/intro/CoffeeLogo';
 
 function App() {
   return (
     <div className="Main">
-		<CoffeeTitle>Coffee Time</CoffeeTitle>
-		<CoffeeLogo src={coffeeLogo} width="300" height="300"></CoffeeLogo>
+		<CoffeeLogo />
     </div>
   );
 }
-
-const animationCoffeeTitle = keyframes`
-	0% { opacity: 0; transform: translateY(-200px); }
-	50% { opacity: 0.5; transform: translateY(0px); }
-	100% { opacity: 1; }
-`
-
-const animationCoffeeLogo = keyframes`
-	0% { opacity: 0; transform: translateY(-200px); }
-	100% { opacity: 1; transofrm: translateY(0px); }
-`
-
-const CoffeeTitle = styled.h1`
-	animation-name: ${animationCoffeeTitle};
-	animation-duration: 2s;
-	animation-fill-mode: forwards;
-`
-
-const CoffeeLogo = styled.img`
-	opacity: 0;
-	animation-name: ${animationCoffeeLogo};
-	animation-duration: 2s;
-	animation-delay: 2s;
-	animation-fill-mode: forwards;
-`
 
 export default App;
---
+++ src/components/intro/CoffeeLogo.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import styled, { keyframes } from 'styled-components';
+
+import coffeeLogoGIF from '../images/GIF/coffee.gif';
+
+export default function CoffeeLogo(){
+	return (
+	<>
+		<CoffeeTitle>Coffee Time</CoffeeTitle>
+		<CoffeeLogoImage src={coffeeLogoGIF} width="300" height="300"></CoffeeLogoImage>
+	</>
+	);
+}
+
+
+const animationCoffeeTitle = keyframes`
+	0% { opacity: 0; transform: translateY(-200px); }
+	50% { opacity: 0.5; transform: translateY(0px); }
+	100% { opacity: 1; }
+`
+
+const animationCoffeeLogo = keyframes`
+	0% { opacity: 0; transform: translateY(-200px); }
+	100% { opacity: 1; transofrm: translateY(0px); }
+`
+
+const CoffeeTitle = styled.h1`
+	@import url('https://fonts.googleapis.com/css2?family=Petemoss&display=swap');
+	
+	
+
+	animation-name: ${animationCoffeeTitle};
+	animation-duration: 2s;
+	animation-fill-mode: forwards;
+	
+	text-shadow: 4px 4px 12px bisque;
+	
+	text-align: center;
+	font-family: 'Petemoss', cursive;
+	font-size: 64px;
+	letter-spacing: 10px;
+	word-spacing: 80px;
+
+	color: chocolate;
+
+	transition-property: letter-spacing, word-spacing;
+	transition-duration: 1s;
+	
+	:hover {
+		letter-spacing: 5px;
+		word-spacing: 40px;
+		cursor: pointer;
+	}
+`
+
+const CoffeeLogoImage = styled.img`
+	opacity: 0;
+	animation-name: ${animationCoffeeLogo};
+	animation-duration: 2s;
+	animation-delay: 2s;
+	animation-fill-mode: forwards;
+	
+	filter: drop-shadow(0px 0px 18px bisque);
+	
+	transition-property: filter, width, height;
+	transition-duration: 0.5s;
+	transition-timing-function: ease-in;
+	
+	:hover {
+	
+		filter: drop-shadow(0px 0px 9px bisque);
+		width: 280px;
+		height: 280px;
+		cursor: pointer;
+		
+	}
+`
