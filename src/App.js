720326208b56cc363196ae7f4e799b55c190941f
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-14 06:45:40 +0200

    change: padding of the logo and intro text

--- src/App.js
+++ src/App.js
@@ -2,11 +2,63 @@ import './App.css';
 
 import CoffeeLogo from './components/intro/CoffeeLogo';
 
+import {
+	BrowserRouter as Router,
+	Routes, 
+	Route,
+	Link
+} from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
 function App() {
   return (
+  <>
     <div className="Main">
 		<CoffeeLogo />
     </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+    </>
   );
 }
 
