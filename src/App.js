2713b2b6d6e460cf1fc126913260670bee868d05
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-19 17:55:30 +0200

    add: better intro

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import './App.css';
 
 import CoffeeLogo from './components/intro/CoffeeLogo';
 
-import {
-	Link
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function App() {
   return (
