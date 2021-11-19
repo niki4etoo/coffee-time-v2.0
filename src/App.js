1a69e2bdfec1a53cfd8d69e3f5a3e624330508f6
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-19 17:52:12 +0200

    feat: Better intro and bigger letter spacing in home content

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import './App.css';
 import CoffeeLogo from './components/intro/CoffeeLogo';
 
 import {
-	BrowserRouter as Router,
-	Routes, 
-	Route,
 	Link
 } from 'react-router-dom';
 
-import styled, { keyframes } from 'styled-components';
-
-function App() {
+export default function App() {
   return (
   <>
   <Link to="/Home">
@@ -22,47 +17,3 @@ function App() {
     </>
   );
 }
-
-const proceedAnimation = keyframes`
-	0% { opacity: 0; }
-	100% { opacity: 1; }
-`
-
-const Proceed = styled.div`
-	margin: 30px auto;
-	width: 200px;
-	
-	text-align: center;
-	text-decoration: none;
-	font-size: 42px;
-	font-family: 'Petemoss', cursive;
-	letter-spacing: 3px;
-	
-	background-color: bisque;
-	color: chocolate;
-	
-	text-shadow: 2px 2px 5px white;
-	
-	animation-name: ${proceedAnimation};
-	animation-duration: 1s;
-	animation-delay: 0.8s;
-	animation-fill-mode: forwards;
-	opacity: 0;	
-	
-	a, a:visited {
-		text-decoration: none;
-		color: chocolate;
-		text-shadow: 2px 2px 5px white;
-		
-		transition-property: letter-spacing, text-shadow;
-		transition-duration: 0.8s;
-	}
-	
-	a:hover {
-		text-decoration: none;
-		letter-spacing: 2px;
-		text-shadow: 1px 1px 3px bisque;
-	}
-`
-
-export default App;
