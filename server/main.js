26e98d44454647f8a002d6724583ba7ae488d01d
Nikolai Nanev <niki4etooo@gmail.com>
2021-11-26 20:42:37 +0200

    add: connection to db

--- server/main.js
+++ server/main.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const mysql = require('mysql2');
+
+const app = express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "todo",
+  password: "todo",
+  port: 3000,
+  database: "coffeetimedb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+})
