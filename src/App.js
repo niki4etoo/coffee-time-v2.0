fc2d311b97ab136e753dc0d06d8b887008a43e3d
Nikolai Nanev <niki4etooo@gmail.com>
2021-12-07 16:52:30 +0200

    fix: removed styled-components and change the style of the navigation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 
 import CoffeeLogo from './components/intro/CoffeeLogo';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 
 fetch('/users/Elaine', {
 		  headers: { 
@@ -30,19 +29,11 @@ export default function App() {
 	
 	return (
 	<>
-		<Logo>
-		<Link to="/Home">
-			<CoffeeLogo />
-		</Link>
-		</Logo>
+		<div className="logo">
+			<Link to="/Home">
+				<CoffeeLogo />
+			</Link>
+		</div>
 	</>
 	);
 }
-
-const Logo = styled.div`
-	text-align: center;
-
-	a, a:hover, a:visited {
-		text-decoration: none;
-	}
-`
