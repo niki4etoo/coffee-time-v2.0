31545eaea3686cde6b457cb52e9d5de260c504d0
Nikolai Nanev <niki4etooo@gmail.com>
2021-12-20 09:45:17 +0200

    added function for frontend usage later on

--- server/main.js
+++ server/main.js
@@ -27,7 +27,29 @@ app.listen(port, () => {
 	console.log(`The server is listening on ${host}:${port}`);
 });
 
-
+function extractInfoFromServer(){
+	fetch('/users/Elaine', {
+		  headers: { 
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		   }
+		})
+	.then(response => {
+			if(response.status === 200){
+				return response.json();
+			}
+	})
+	.then(data => {
+		for(var d of data){
+			console.log("id: " + d.id);
+			console.log("name: " + d.name);
+			console.log("email: " + d.email);
+			
+		}
+	}).catch(error => {
+		console.log(error);
+	});
+}
 
 
 
