fbb600a97854cd9da4f2bc41857aa71cbecdc635
Nikolai Nanev <niki4etooo@gmail.com>
2022-01-18 20:58:24 +0200

    reduced some info for the coffee drinks

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -29,11 +29,6 @@ export default function HomeContainer(){
 				  Outside of Italy, cappuccino is a coffee drink that today is typically composed of a single espresso shot and hot milk,
 				   with the surface topped with foamed milk. Cappuccinos are most often prepared with an espresso machine.
 				  </p>
-				  <p>
-				   The espresso is poured into the bottom of the cup, followed by a similar amount of hot milk, which is prepared by heating
-				    and texturing the milk using the espresso machine steam wand. The top third of the drink consists of milk foam.
-				     This foam can be decorated with artistic drawings made with the same milk, called latte art.
-				  </p>
 				</div>
 				<div className="right">
 					<p>
@@ -43,9 +38,6 @@ export default function HomeContainer(){
 					<p>
 					Variations of the drink involve the use of cream instead of milk,
 					 using non-dairy milk substitutes and flavoring with cinnamon or chocolate powder.
-					</p>
-					<p>Caffè Americano is a type of coffee drink prepared by diluting an espresso with hot water, giving it a similar strength to,
-					 but different flavor from, traditionally brewed coffee.
 					</p>
 					<img src={coffeeImage4} width="300" height="300" alt="coffee with foam" />
 					<p>
